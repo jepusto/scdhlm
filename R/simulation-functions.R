8a882c44578ad739a03854a6cf183042a45b4362
James E. Pustejovsky <jepusto@gmail.com>
2013-12-31 14:58:37 -0600

    Fixed centering for model MB4.

--- R/simulation-functions.R
+++ R/simulation-functions.R
@@ -253,7 +253,7 @@ simulate_MB2 <- function(iterations, beta, rho, phi, tau1_ratio, tau_corr, desig
 ## Simulate model MB4 
 ##------------------------------------------------------------------------
 
-convergence_handler_MB4 <- function(design, y, p_const) {
+convergence_handler_MB4 <- function(design, y, p_const, r_const) {
   fixed_terms <- c("constant","treatment","trend","interaction")
   W <- NULL
   m_full <- withCallingHandlers(
@@ -264,7 +264,7 @@ convergence_handler_MB4 <- function(design, y, p_const) {
   attr(m_full, "warning") <- W
   
   if (!inherits(m_full,"error")) {
-    g <- g_REML(m_full, p_const=p_const, r_const=c(1,0,1,0,0),
+    g <- g_REML(m_full, p_const=p_const, r_const=r_const,
                 X_design=as.matrix(design[,fixed_terms]), 
                 Z_design=as.matrix(design[,c("constant","trend")]),
                 block=design$id, times=NULL, returnModel=FALSE)[-12]
@@ -292,6 +292,7 @@ convergence_handler_MB4 <- function(design, y, p_const) {
 #' @param tau2_ratio ratio of trend variance to intercept variance 
 #' @param tau_corr correlation between case-specific trends and intercepts
 #' @param p_const vector of constants for calculating numerator of effect size
+#' @param r_const vector of constants for calculating denominator of effect size
 #' @param design design matrix. If not specified, it will be calculated based on \code{m}, \code{n}, and \code{MB}.
 #' @param m number of cases. Not used if \code{design} is specified.
 #' @param n number of measurement occasions. Not used if \code{design} is specified.
@@ -310,13 +311,18 @@ convergence_handler_MB4 <- function(design, y, p_const) {
 #' 
 #' @examples
 #' simulate_MB4(iterations = 10, beta = c(0,1,0,0), rho = 0.8, phi = 0.5, tau2_ratio = 0.5, tau_corr = 0, 
-#'              p_const = c(0,1,0,7), design = design_matrix(3, 16, treat_times=c(5,9,13), center = 12))
+#'              p_const = c(0,1,0,7), r_const = c(1,0,1,0,0), design = design_matrix(3, 16, treat_times=c(5,9,13), center = 12))
+#' simulate_MB4(iterations = 10, beta = c(0,1,0,0), rho = 0.8, phi = 0.5, tau2_ratio = 0.5, tau_corr = 0, 
+#'              m = 6, n = 8)
 
-simulate_MB4 <- function(iterations, beta, rho, phi, tau2_ratio, tau_corr, p_const, design, m, n, MB = TRUE) {
-  
+simulate_MB4 <- function(iterations, beta, rho, phi, tau2_ratio, tau_corr, 
+                         p_const, r_const, design, m, n, MB = TRUE) {
+
   if (missing(design)) {
+    if (missing(p_const)) p_const <- c(0,1,0, n/4)
+    if (missing(r_const)) r_const <- c(1,0,1,0,0)
     treat_times <- if (MB) MBTreatTimes(m=m,n=n) else rep(n / 2 + 1, m)
-    design <- design_matrix(m=m, n=n, treat_times = treat_times)
+    design <- design_matrix(m=m, n=n, treat_times = treat_times, center = 3*n/4)
   }
   
   Tau <- rbind(cbind(
@@ -324,7 +330,8 @@ simulate_MB4 <- function(iterations, beta, rho, phi, tau2_ratio, tau_corr, p_con
     matrix(0,2,2)), matrix(0,2,4))[c(1,3,2,4),c(1,3,2,4)]
   
   y_sims <- simulation_data(iterations, design, beta, sigma_sq = 1 - rho, phi, Tau = Tau)
-  RML <- apply(y_sims, 2, convergence_handler_MB4, design=design, p_const=p_const)
+  RML <- apply(y_sims, 2, convergence_handler_MB4, 
+               design=design, p_const=p_const, r_const=r_const)
   RML_mat <- matrix(unlist(RML), length(unlist(RML[[1]])), iterations, 
                     dimnames = list(names(unlist(RML[[1]]))))
   RML_coverage1 <- coverage(delta = sum(beta * p_const), CI = CI_SMD(delta = RML_mat["delta_AB",], kappa = RML_mat["kappa",], nu = RML_mat["nu",]))
