ebf0f6e654eb80c6b30d49d61076c73c900f5744
James E. Pustejovsky <jepusto@gmail.com>
2013-12-31 20:22:11 -0600

    Removed analysis from simulation demos.

--- demo/Simulation-MB1.R
+++ demo/Simulation-MB1.R
@@ -2,7 +2,7 @@ set.seed(1)
 #------------------------------------
 # Design parameters
 #------------------------------------
-iterations <- 10000
+iterations <- 20
 beta <- c(0,1,0,0)
 phi <- seq(-7L, 7L, 2) / 10L
 rho <- seq(0.0, 0.8, 0.2)
@@ -18,42 +18,36 @@ head(parms)
 # run simulations in parallel on Windows via SNOW
 #--------------------------------------------------------
 
-# library(plyr)
-# library(snow)
-# library(foreach)
-# library(iterators)
-# library(doSNOW)
-# library(rlecuyer)
-# 
-# cluster <- makeCluster(8, type = "SOCK")
-# registerDoSNOW(cluster)
-# 
-# # set up parallel random number generator
-# clusterSetupRNGstream(cluster, 20131220)
-# 
-# # execute simulations
-# system.time(MB1_array <- maply(parms, .fun = compare_RML_HPS, 
-#                              iterations = iterations, beta = beta, 
-#                              .drop=FALSE, .parallel=TRUE,
-#                              .paropts = list(.packages="scdhlm")))
-# stopCluster(cluster)
-# 
-# 
-# ##------------------------------------------------
-# ## reshape and save results
-# ##------------------------------------------------
-# library(reshape)
-# 
-# names(dimnames(MB1_array))[7:8] <- c("stat","moment")
-# dimnames(MB1_array)$stat[c(5,19)] <- c("kappa_RML","kappa_HPS")
-# 
-# MB1results <- cast(melt(MB1_array), ... ~ moment)
-# attr(MB1results, "iterations") <- iterations
-# attr(MB1results, "beta") <- beta
-# save(MB1results, file="data/MB1results.RData", compress="xz")
+library(plyr)
+library(parallel)
+library(foreach)
+library(iterators)
+library(doSNOW)
+library(rlecuyer)
+
+cluster <- makeCluster(detectCores(), type = "SOCK")
+registerDoSNOW(cluster)
+
+# set up parallel random number generator
+clusterSetupRNGstream(cluster, 20131220)
+
+# execute simulations
+system.time(MB1_array <- maply(parms, .fun = compare_RML_HPS, 
+                             iterations = iterations, beta = beta, 
+                             .drop=FALSE, .parallel=TRUE,
+                             .paropts = list(.packages="scdhlm")))
+stopCluster(cluster)
 
 
-#--------------------------------------
-# Analyze results
-#--------------------------------------
-data(MB1results)
+##------------------------------------------------
+## reshape and save results
+##------------------------------------------------
+library(reshape)
+
+names(dimnames(MB1_array))[7:8] <- c("stat","moment")
+dimnames(MB1_array)$stat[c(5,19)] <- c("kappa_RML","kappa_HPS")
+
+MB1results <- cast(melt(MB1_array), ... ~ moment)
+attr(MB1results, "iterations") <- iterations
+attr(MB1results, "beta") <- beta
+save(MB1results, file="data/MB1results.RData", compress="xz")
--- demo/Simulation-MB4.R
+++ demo/Simulation-MB4.R
@@ -2,13 +2,12 @@ set.seed(1)
 #------------------------------------
 # Design parameters
 #------------------------------------
-iterations <- 10000
+iterations <- 20000
 beta <- c(0,1,0,0)
 phi <- seq(-7L, 7L, 2) / 10L
 rho <- seq(0.0, 0.8, 0.2)
 tau2_ratio <- c(0.1, 0.5)
 tau_corr <- 0
-p_const <- c(0,1,0,7)
 m <- c(3,4,5,6,9,12)
 n <- c(8, 16)
 
@@ -18,46 +17,40 @@ parms <- expand.grid(phi = phi, rho = rho, tau2_ratio = tau2_ratio, tau_corr = t
 head(parms)
 
 
-# #--------------------------------------------------------
-# # run simulations in parallel on Windows via SNOW
-# #--------------------------------------------------------
-# 
-# library(plyr)
-# library(snow)
-# library(foreach)
-# library(iterators)
-# library(doSNOW)
-# library(rlecuyer)
-# 
-# cluster <- makeCluster(4, type = "SOCK")
-# registerDoSNOW(cluster)
-# 
-# # set up parallel random number generator
-# clusterSetupRNGstream(cluster, 20131003)
-# 
-# # execute simulations
-# system.time(MB4_array <- maply(parms, .fun = simulate_MB4, 
-#                                 iterations = iterations, beta = beta, p_const = p_const,
-#                                 .drop=FALSE, .parallel=TRUE,
-#                                 .paropts = list(.packages="scdhlm")))
-# stopCluster(cluster)
-# 
-# 
-# ##------------------------------------------------
-# ## reshape and save results
-# ##------------------------------------------------
-# 
-# library(reshape)
-# 
-# names(dimnames(MB4_array))[7:8] <- c("stat","moment")
-# MB4results <- cast(melt(MB4_array), ... ~ moment)
-# attr(MB4results, "iterations") <- iterations
-# attr(MB4results, "beta") <- beta
-# attr(MB4results, "p_const") <- p_const
-# save(MB4results, file="data/MB4results.RData", compress="xz")
+#--------------------------------------------------------
+# run simulations in parallel on Windows via SNOW
+#--------------------------------------------------------
+
+library(plyr)
+library(parallel)
+library(foreach)
+library(iterators)
+library(doSNOW)
+library(rlecuyer)
+
+cluster <- makeCluster(detectCores(), type = "SOCK")
+registerDoSNOW(cluster)
+
+# set up parallel random number generator
+clusterSetupRNGstream(cluster, 20131003)
+
+# execute simulations
+system.time(MB4_array <- maply(parms, .fun = simulate_MB4, 
+                                iterations = iterations, beta = beta, p_const = p_const,
+                                .drop=FALSE, .parallel=TRUE,
+                                .paropts = list(.packages="scdhlm")))
+stopCluster(cluster)
 
 
-#--------------------------------------
-# Analyze results
-#--------------------------------------
-data(MB4results)
+##------------------------------------------------
+## reshape and save results
+##------------------------------------------------
+
+library(reshape)
+
+names(dimnames(MB4_array))[7:8] <- c("stat","moment")
+MB4results <- cast(melt(MB4_array), ... ~ moment)
+attr(MB4results, "iterations") <- iterations
+attr(MB4results, "beta") <- beta
+attr(MB4results, "p_const") <- p_const
+save(MB4results, file="data/MB4results.RData", compress="xz")
