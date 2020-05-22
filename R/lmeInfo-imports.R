9d1def016c55454a4f1101d690dc3f2002b85bf4
Man Chen <amandachen@utexas.edu>
2020-05-22 10:32:29 -0500

    Imported CI_g from lmeInfo.

--- R/lmeInfo-imports.R
+++ R/lmeInfo-imports.R
@@ -1,6 +1,7 @@
 #' @importFrom lmeInfo extract_varcomp
 #' @importFrom lmeInfo varcomp_vcov
 #' @importFrom lmeInfo g_mlm
+#' @importFrom lmeInfo CI_g
 
 #' @title lmeInfo
 #' @description 
@@ -10,11 +11,12 @@
 #'   \item \code{\link[lmeInfo]{extract_varcomp}}
 #'   \item \code{\link[lmeInfo]{g_mlm}}
 #'   \item \code{\link[lmeInfo]{varcomp_vcov}}
+#'   \item \code{\link[lmeInfo]{CI_g}}
 #' }
 #' 
 #' @name lmeInfo
-#' @aliases extract_varcomp varcomp_vcov g_mlm
-#' @export extract_varcomp g_mlm varcomp_vcov
+#' @aliases extract_varcomp varcomp_vcov g_mlm CI_g
+#' @export extract_varcomp g_mlm varcomp_vcov CI_g
 #' @md
 #' 
 NULL
