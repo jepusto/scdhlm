628b29383f6047654f2305eb5cd29bea129f9a7c
Man Chen <amandachen@utexas.edu>
2020-09-08 18:31:04 -0500

    Added app.R file. This allows us to remove the source() functions in ui.R and server.R.

---
+++ inst/shiny-examples/scdhlm/app.R
@@ -0,0 +1,7 @@
+library(shiny)
+
+source("helper-functions.R", local = TRUE)
+source("lme-fit.R", local = TRUE)
+source("mappings.R", local = TRUE)
+source("server.R", local = TRUE)
+source("ui.R", local = TRUE)
