79c7726a0ee5dcf1fb611cbc0e17f341cee8230f
Man Chen <amandachen@utexas.edu>
2020-09-09 08:28:40 -0500

    fiddling

--- inst/shiny-examples/scdhlm/app.R
+++ inst/shiny-examples/scdhlm/app.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-# source("helper-functions.R", local = TRUE)
+source("helper-functions.R", local = TRUE)
 source("lme-fit.R", local = TRUE)
 source("mappings.R", local = TRUE)
 source("server.R", local = TRUE)
