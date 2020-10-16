78e1c1bcce68f5f9b0cede2bdd0209140a82a383
Man Chen <amandachen@utexas.edu>
2020-10-15 22:29:29 -0500

    Added r_const calculation to es-RML code chunk.

--- inst/shiny-examples/scdhlm/code-chunks/es-RML.R
+++ inst/shiny-examples/scdhlm/code-chunks/es-RML.R
@@ -3,6 +3,7 @@ A <- {user_A}
 B <- {user_B}
 p_const <- c(rep(0L, length({user_FE_base})), (B - A - 1)^as.integer({user_FE_trt})) 
 p_const <- {user_pconstant}
+r_const <- c({user_rconst_base}, {user_rconst_trt}, {user_rconst_cor}, {user_rconst_var}, 1L) # specify whether using random effects, cor struct, var struct, and level-1 errors
 r_const <- {user_rconstant}
 ES_RML <- g_mlm(fit_RML, p_const = p_const, r_const = r_const, infotype = "expected", returnModel = TRUE)
 summary(ES_RML)
