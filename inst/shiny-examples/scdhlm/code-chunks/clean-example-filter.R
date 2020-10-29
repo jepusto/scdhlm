799fcdc324044dcf338f0e12d8c150beb6a20912
Man Chen <amandachen@utexas.edu>
2020-10-28 23:42:23 -0500

    Updated relevant code-chunks.

--- inst/shiny-examples/scdhlm/code-chunks/clean-example-filter.R
+++ inst/shiny-examples/scdhlm/code-chunks/clean-example-filter.R
@@ -1,3 +1,3 @@
 # filter data
-dat <- subset(dat, {user_filtervars} == "{user_filtervals}")
+dat <- subset(dat, {user_filterString})
 
--- inst/shiny-examples/scdhlm/code-chunks/clean-inputdata-filter.R
+++ inst/shiny-examples/scdhlm/code-chunks/clean-inputdata-filter.R
@@ -1,3 +1,2 @@
 # filter data
-dat <- subset(dat, {user_filtervars} == "{user_filtervals}")
-
+dat <- subset(dat, {user_filterString})
