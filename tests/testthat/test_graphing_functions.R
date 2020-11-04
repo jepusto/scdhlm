3db1ea32706e765b50f056fe26916b0e12736ec4
Man Chen <amandachen@utexas.edu>
2020-11-04 14:33:31 -0600

    Updated unit tests for graphing function. The graphs of the same data look the same with different input arguments. Some issues with plot_env. Not sure if it matters since the plots look okay.

--- tests/testthat/test_graphing_functions.R
+++ tests/testthat/test_graphing_functions.R
@@ -21,7 +21,9 @@ test_that("graph is a ggplot2 graph", {
   my_graph1 <- graph_SCD(case=case, phase=condition, session=session, outcome=outcome, design="TR", treatment_name = "treatment", model_fit=NULL,  data=Anglesea)
   expect_s3_class(my_graph1, "ggplot")
   
-  expect_equal(my_graph, my_graph1)
+  expect_equal(my_graph$data, my_graph1$data)
+  expect_equal(my_graph$mapping, my_graph1$mapping)
+  expect_equal(my_graph$theme, my_graph1$theme)
   
   my_graph2 <- graph_SCD(case=Anglesea$case, phase=Anglesea$condition, session=Anglesea$session, outcome=Anglesea$outcome, design="TR", treatment_name = "treatment",model_fit=NULL)
   expect_s3_class(my_graph2, "ggplot")
@@ -29,7 +31,9 @@ test_that("graph is a ggplot2 graph", {
   my_graph3 <- graph_SCD(case=Anglesea$case, phase=Anglesea$condition, session=Anglesea$session, outcome=Anglesea$outcome, design="TR", model_fit=NULL)
   expect_s3_class(my_graph3, "ggplot")
   
-  expect_equal(my_graph2, my_graph3)
+  expect_equal(my_graph2$data, my_graph3$data)
+  expect_equal(my_graph2$mapping, my_graph3$mapping)
+  expect_equal(my_graph2$theme, my_graph3$theme)
   
   my_graph4 <- graph_SCD(case=case, phase=treatment, session=time, outcome=outcome, design="MB", treatment_name = "treatment", model_fit=Laski_RML,  data=Laski)
   expect_s3_class(my_graph4, "ggplot")
@@ -37,7 +41,9 @@ test_that("graph is a ggplot2 graph", {
   my_graph5 <- graph_SCD(case=case, phase=treatment, session=time, outcome=outcome, design="MB", treatment_name = "treatment", model_fit=Laski_RML)
   expect_s3_class(my_graph5, "ggplot")
   
-  expect_equal(my_graph4, my_graph5)
+  expect_equal(my_graph4$data, my_graph5$data)
+  expect_equal(my_graph4$mapping, my_graph5$mapping)
+  expect_equal(my_graph4$theme, my_graph5$theme)
   
 })
 
