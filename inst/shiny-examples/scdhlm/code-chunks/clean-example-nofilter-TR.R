c5bf094385f0e52cb687c994b82748fe39e0a96f
Man Chen <amandachen@utexas.edu>
2020-11-04 14:36:27 -0600

    Moved data argument to the end of the preprocess_SCD() in those code chunks.

--- inst/shiny-examples/scdhlm/code-chunks/clean-example-nofilter-TR.R
+++ inst/shiny-examples/scdhlm/code-chunks/clean-example-nofilter-TR.R
@@ -1,6 +1,6 @@
 # Clean data
 dat <- dat[,c("{user_parms}")]
 names(dat) <- c("case","session","phase","outcome")
-dat <- preprocess_SCD(data = dat, case = case, phase = phase, 
-                      session = session, outcome = outcome, design = "{user_design}")
+dat <- preprocess_SCD(case = case, phase = phase, session = session, outcome = outcome, 
+                      design = "{user_design}", data = dat)
 
