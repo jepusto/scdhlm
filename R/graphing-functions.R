23523b7fb5006c56290f061cc2d9e03dfc1dfb37
Man Chen <amandachen@utexas.edu>
2020-11-04 14:26:26 -0600

    Updated graphing function using preprocess_SCD(). Renamed variables bc phase_lines_by_case() needs the variables to be "case", "phase", and "session". Could consider update this function if not renaming.

--- R/graphing-functions.R
+++ R/graphing-functions.R
@@ -66,14 +66,14 @@ phase_lines_by_case <- function(x) {
 #' 
 #' @description Graphs single case design data for treatment reversal and multiple baseline designs. 
 #' 
-#' @param outcome vector of outcome data or name of variable within \code{data}. May not contain any missing values.
-#' @param phase vector of treatment indicators or name of variable within \code{data}. Must be the same length as \code{outcome}.
-#' @param case factor vector indicating unique cases or name of variable within \code{data}. Must be the same length as \code{outcome}.
-#' @param session vector of measurement occasion times or name of variable within \code{data}. Must be the same length as \code{outcome}.
-#' @param data (Optional) dataset to use for analysis. Must be data.frame. 
-#' @param design Specify whether it is a treatment reversal, "TR", or multiple baseline, "MB", design
-#' @param treatment_name (Optional) value of the name of the treatment phase
+#' @param case vector of case indicators or name of a character or factor vector within \code{data} indicating unique cases.
+#' @param phase vector of treatment indicators or name of a character or factor vector within \code{data} indicating unique treatment phases.
+#' @param session vector of measurement occasions or name of numeric vector within \code{data} of measurement times.
+#' @param outcome vector of outcome data or name of numeric vector of outcome data within \code{data}.
+#' @param design Character string to specify whether data comes from a treatment reversal, "TR", or multiple baseline, "MB", design.
+#' @param treatment_name (Optional) character string corresponding to the name of the treatment phase.
 #' @param model_fit (Optional) lme fitted model that adds predicted values to graph
+#' @param data (Optional) dataset to use for analysis. Must be a \code{data.frame}.
 #' 
 #' @note If treatment_name is left null it will choose the second level of the phase variable to be the treatment phase.
 #' 
@@ -96,69 +96,55 @@ phase_lines_by_case <- function(x) {
 #'           data=BartonArwood)
 #' 
 
-graph_SCD <- function(case, phase, session, outcome, design, treatment_name = NULL, model_fit=NULL, data=NULL) {
+graph_SCD <- function(case, phase, session, outcome, design, treatment_name = NULL, model_fit = NULL, data = NULL) {
   
   phase_pair <-  phase_time  <- NULL
   
-  if (!is.null(data) | !is.null(model_fit)) {
-    outcome_call <- substitute(outcome)
-    phase_call <- substitute(phase)
-    case_call <- substitute(case)
-    session_call <- substitute(session)
-    
-    if (is.null(data)) {
-      data <- nlme::getData(model_fit)
-    } 
-    
-    env <- list2env(data, parent = parent.frame())
-    outcome <- eval(outcome_call, env)
-    phase <- eval(phase_call, env)
-    case <- eval(case_call, env)
-    session <- eval(session_call, env)
-    
-  }
-
-  if (is.null(treatment_name)) {
-    treatment_name <-  levels(as.factor(phase))[2]
+  case_call <- substitute(case)
+  phase_call <- substitute(phase)
+  session_call <- substitute(session)
+  outcome_call <- substitute(outcome)
+  
+  if (!is.null(model_fit) & is.null(data)) {
+    data <- nlme::getData(model_fit)
   }
   
-  dat <- data.frame(case = factor(case),
-                    phase = factor(phase),
-                    session_fac = factor(session),
-                    outcome, session)
-  trt_phase <- treatment_name
-  dat$trt <- as.numeric(dat$phase==trt_phase)
+  dat <- do.call(preprocess_SCD,
+                 args = list(case = case_call, 
+                             phase = phase_call,
+                             session = session_call,
+                             outcome = outcome_call, 
+                             design = design,
+                             treatment_name = treatment_name,
+                             data = data))
+  names(dat)[1:4] <- c("case", "phase", "session", "outcome")
+  
   phase_line_dat <- phase_lines_by_case(dat)
-  
-  if (design == "MB") { 
-    dat$session_trt <- unlist(by(dat, dat$case, session_by_treatment, trt_phase = trt_phase))
-  } else {
-    dat$phase_pair <- unlist(by(dat, dat$case, phase_pairs))
-  }
-  
+
   if (design=="MB") {
-    p <- ggplot2::ggplot(dat, ggplot2::aes(session, outcome, color = as.factor(phase), shape = as.factor(phase)))  
+    p <- ggplot2::ggplot(dat, ggplot2::aes(session, outcome, color = phase, shape = phase))
   } else {
+    names(dat)[6] <- "phase_pair"
     p <- ggplot2::ggplot(dat, ggplot2::aes(session, outcome, color = phase, shape = phase, group = interaction(phase, phase_pair)))
   }
-  
-  p <- p + 
-    ggplot2::geom_point() + 
-    ggplot2::geom_line() + 
-    ggplot2::facet_grid(case ~ .) + 
-    ggplot2::theme_bw() + 
-    ggplot2::labs(color = "", shape = "") + 
+
+  p <- p +
+    ggplot2::geom_point() +
+    ggplot2::geom_line() +
+    ggplot2::facet_grid(case ~ .) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(color = "", shape = "") +
     ggplot2::geom_vline(data = phase_line_dat, ggplot2::aes(xintercept = phase_time), linetype = "dashed")
-  
+
   # With model fit
   if (!is.null(model_fit)) {
-    
+
     dat$fitted <- predict(model_fit)
-    
+
     p <- p + ggplot2::geom_line(data = dat, ggplot2::aes(session, fitted), size = 0.8)
-    
-  } 
-  
+
+  }
+
   p
   
 }
